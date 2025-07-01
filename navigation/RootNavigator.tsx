f2b0d4de6d81b97f9e198a62c3cf1b2b9514c616
mcquest <80595817+mcquest@users.noreply.github.com>
2025-07-01 15:03:08 -0700

    Update RootNavigator.tsx
    favorites

--- navigation/RootNavigator.tsx
+++ navigation/RootNavigator.tsx
@@ -4,10 +4,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SearchScreen from '../screens/SearchScreen';
 import DetailScreen from '../screens/DetailScreen';
 import { Track } from '../types';
+import FavoritesScreen from '../screens/FavoritesScreen';
+
 
 export type RootStackParamList = {
   Search: undefined;
   Details: { track: Track };
+  Favorites: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -15,9 +18,11 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 const RootNavigator = () => (
   <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="Search" component={SearchScreen} />
-      <Stack.Screen name="Details" component={DetailScreen} />
-    </Stack.Navigator>
+  <Stack.Screen name="Search" component={SearchScreen} />
+  <Stack.Screen name="Details" component={DetailScreen} />
+  <Stack.Screen name="Favorites" component={FavoritesScreen} />
+</Stack.Navigator>
+
   </NavigationContainer>
 );
 
